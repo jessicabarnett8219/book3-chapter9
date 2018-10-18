2081d3d9f1750f07f75e9638adc6d129cdd3418a
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-10-18 15:07:49 -0500

    made test functions for creating elements and adding to the DOM

--- scripts/foodapi.js
+++ scripts/foodapi.js
@@ -1,5 +1,41 @@
-fetch("http://localhost:8088/food")
-    .then(foods => foods.json())
-    .then(parsedFoods => {
-        console.table(parsedFoods)
-    })
+// fetch("http://localhost:8088/food")
+//     .then(foods => foods.json())
+//     .then(parsedFoods => {
+//         console.table(parsedFoods)
+//     })
+
+let pizza = {
+  name: "pizza",
+  type: "dinner",
+  ethnicity: "italian"
+}
+  
+const foodList = document.querySelector(".foodList")
+
+const createFoodItem = (foodObject) => {`
+<section class="foodItem">
+    <h2>${foodObject}.name</h2>
+    <p>${foodObject}.type/p>
+    <p>${foodObject}.ethnicity</p>
+</section>
+`
+}
+
+const makeFoodItem = (foodName) => { 
+  let foodItem = document.createElement("section")
+  foodItem.innerHTML = `<h1>${foodName}</h1>`
+  foodList.appendChild(foodItem)
+}
+
+makeFoodItem("pizza")
+// const insertFoodItem = (pizza) => {
+//   let foodItem = createFoodItem(pizza)
+//   foodList.appendChild(foodItem)
+//   return foodItem
+// }
+
+
+
+
+
+// Create a function that inserts an HTML representation of a food into the DOM
